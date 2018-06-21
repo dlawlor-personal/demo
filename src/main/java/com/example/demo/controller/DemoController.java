dc24289bcfead549dc3d54061247144d137db4ff
declanlawlor <declan@donedeal.ie>
2018-06-21 23:02:54 +0100

    add ontroller

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,4 +1,13 @@
 package com.example.demo.controller;
 
-public class DemoControllre {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/rest")
+public class DemoController {
+
+    @RequestMapping("/index")
+    public String hello () {
+        return "hellow";
+    }
 }
