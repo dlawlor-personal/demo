0bdd633b78930c0aedc2f12d3ca7acd2be5af592
declanlawlor <declan@donedeal.ie>
2018-06-22 09:43:49 +0100

    update

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,13 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/rest")
 public class DemoController {
 
-    @RequestMapping("/index")
+    @GetMapping("/index")
     public String hello () {
-        return "hellow";
+        return "hello";
     }
 }
